502113c17e3ce71a7d1acf2d117b4f0402d6b328
Enrique Alonso Miramontes Tiznado <enalmiramontesti@ittepic.edu.mx>
2021-11-12 19:28:07 -0700

    Hola

--- app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/BaseDatos.kt
+++ app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/BaseDatos.kt
@@ -1,4 +1,11 @@
 package tepic.tecnm.mx.eamtlmhm.ladm_u3_practica2_firebasefirestore
 
-class BaseDatos {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BaseDatos(context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int) :
+    SQLiteOpenHelper(context,name,factory,version) {
+
+
 }
