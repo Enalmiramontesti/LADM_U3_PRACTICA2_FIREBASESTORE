344415780dc35830412dab433fd3d503821a1476
Enrique Alonso Miramontes Tiznado <enalmiramontesti@ittepic.edu.mx>
2021-11-12 20:25:52 -0700

    Se agrego fun selectAll e insert

--- app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/BaseDatos.kt
+++ app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/BaseDatos.kt
@@ -7,5 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper
 class BaseDatos(context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int) :
     SQLiteOpenHelper(context,name,factory,version) {
 
+    override fun onCreate(db: SQLiteDatabase?) {
+        db!!.execSQL("CREATE TABLE NOTA (ID INTEGER PRIMARY KEY  AUTOINCREMENT, TITULO VARCHAR(50), CONTENIDO VARCHAR(300), HORA TIME, FECHA DATE)")
+    }//onCreate
 
-}
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+    }//end onUpgrade
+
+}//end class
