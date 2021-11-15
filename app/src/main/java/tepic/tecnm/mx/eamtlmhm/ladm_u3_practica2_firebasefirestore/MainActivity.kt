2803cabc6363cb197125d090b07d0407cdf35d72
luisicho <lumihernadezma@ittepic.edu.mx>
2021-11-14 23:18:58 -0700

    Ultimo commit con firebase funcionando

--- app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/MainActivity.kt
+++ app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rvEscalar: RecyclerView
     private lateinit var adaptador: EscalonarRecicler
 
-    val timer = object  : CountDownTimer(20000,200){
+    val timer = object  : CountDownTimer(20000,1000){
         override fun onTick(p0: Long) {
             iniciarComponentes()
         }
@@ -34,90 +34,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(view)
         }///insertar
         sincronizar.setOnClickListener {
-            if (!escalalista.isEmpty()){
-                //Devuelve la lista que encontro en la nube de los datos
-                var listaFirebase = Nota(this).selectAllFirestore()
-                //Devuelve la lista de datos que se insertaran
-                var listaFirebaseAInsertar = comparaInsert(listaFirebase,escalalista)
-                //Devuelve la lista de datos que se modificaran
-                var listaFirebaseAModificar = comparaUpdate(listaFirebase,escalalista)
-                //Devuelve la lista de datos que se borraran
-                var listaFirebaseABorrar = comparaDelete(listaFirebase,escalalista)
-
-                //Insertamos firebase
-                listaFirebaseAInsertar.forEach { it.insertarFirebase() }
-                //Modificamos firebase
-                listaFirebaseAModificar.forEach { it.update(it.id.toString()) }
-                //Eliminamos firebase
-                listaFirebaseABorrar.forEach { it.delete(it.id.toString()) }
-                Toast.makeText(this,"SE SINCRONIZO CON EXITO",Toast.LENGTH_LONG)
-            }else{
-                Toast.makeText(this,"Agrege una nota para sincronizar",Toast.LENGTH_LONG)
-            }
+            //if (!escalalista.isEmpty()){
+                //Actualiza elimina e inserta en nube
+                Nota(this).selectAllFirestore()
+            /*}else{
+                Toast.makeText(this,"Agrege una nota para sincronizar",Toast.LENGTH_LONG).show()
+            }*/
         }
         timer.start()
     }//en onCreate
 
-    fun comparaDelete(listaFirebase: ArrayList<Nota>, escalalista: ArrayList<Nota>): ArrayList<Nota> {
-        var listaDelete = ArrayList<Nota>()
-        //Compara las listas para ver que dato se eliminara
-        listaFirebase.forEach {
-            var notaLeida = it
-            //Revisa las notas que se eliminaran
-            (0..escalalista.size - 1).forEach {
-                var bandera = false
-                //Compara si id de firebase existe en LOCAL
-                if (notaLeida.id == escalalista[it].id) {
-                    //Coloca bandera true si encontro el id en el arreglo LOCAL
-                    bandera = true
-                }
-                if (!bandera) {
-                    //Se agrega la nota al arreglo cuando no se encontro en local pero si en firebase
-                    listaDelete.add(notaLeida)
-                }
-            }
-        }
-        return listaDelete
-    }
-
-    fun comparaUpdate(listaFirebase: ArrayList<Nota>, escalalista: ArrayList<Nota>): ArrayList<Nota> {
-        var listaUpdate = ArrayList<Nota>()
-        //Compara las listas para ver que dato se modificara
-        listaFirebase.forEach {
-            var notaLeida = it
-            //Revisa las notas que se actualizaran
-            (0..escalalista.size-1).forEach{
-                //Compara si titulo o contenido son diferentes
-                if(notaLeida.titulo != escalalista[it].titulo || notaLeida.contenido != escalalista[it].contenido){
-                    //Se agrega la nota al arreglo cuando se encontro una modificacion
-                    listaUpdate.add(notaLeida)
-                }
-            }
-        }
-        return listaUpdate
-    }
-
-    fun comparaInsert(listaFirebase: ArrayList<Nota>, escalalista: ArrayList<Nota>): ArrayList<Nota> {
-        var listaInsert = ArrayList<Nota>()
-        //Compara las listas para ver que dato se insertara
-        escalalista.forEach {
-            var notaLeida = it
-            //Revisa las notas que se insertaran
-            (0..listaFirebase.size-1).forEach{
-                var bandera = false
-                //Compara si id de LOCAL existe en Firebase
-                if(notaLeida.id == listaFirebase[it].id){
-                    //Coloca bandera true si encontro el id en el arreglo Firebase
-                    bandera = true
-                }
-                if (!bandera){
-                    //Se agrega la nota al arreglo cuando no se encontro en Firebase pero si en local
-                    listaInsert.add(notaLeida)
-                }
-            }
-        }
-        return listaInsert
-    }
 
     private fun iniciarComponentes() {
         escalalista.clear()
--- app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/Nota.kt
+++ app/src/main/java/tepic/tecnm/mx/eamtlmhm/ladm_u3_practica2_firebasefirestore/Nota.kt
@@ -2,6 +2,8 @@ package tepic.tecnm.mx.eamtlmhm.ladm_u3_practica2_firebasefirestore
 
 import android.content.ContentValues
 import android.content.Context
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlin.collections.ArrayList
@@ -13,6 +15,7 @@ class Nota(p:Context) {
     var hora =  ""
     var fecha = ""
     var id : Int? = null
+    var idFirestore = ""
     var pointer = p
 
 
@@ -76,6 +79,15 @@ class Nota(p:Context) {
         return notas
     }//end selectAll
 
+    fun consultNota(identificador:Int):Boolean{
+        val tabla = BaseDatos(pointer,"BDNOTAS",null,1).readableDatabase
+        val cursor = tabla.rawQuery("SELECT * FROM NOTA WHERE ID = ${identificador}",null)
+        if(cursor.moveToFirst()){
+            return true
+        }//end if
+        return false
+    }//end consultNota
+
     fun delete(id: String): Boolean{
         if(BaseDatos(pointer,"BDNOTAS",null,1).writableDatabase.delete("NOTA","ID=?",arrayOf(id)) == 0)
             return false
@@ -94,73 +106,48 @@ class Nota(p:Context) {
             return false
         return true
     }//end update
+
+    fun toString2():String{
+        return "ID = ${id} titulo = ${titulo} contenido = ${contenido}"
+    }
 //-------------------------------------FIREBASE----------------------------------
     var baseRemota = FirebaseFirestore.getInstance()
-    fun insertarFirebase():Boolean {
-        var resultado = false
+    fun insertarFirebase() {
         var datosInsertar = hashMapOf(
-            "ID" to id,
             "TITULO" to titulo,
             "CONTENIDO" to contenido,
             "HORA" to hora,
             "FECHA" to fecha
         )
         baseRemota.collection("NOTAS")
-            .add(datosInsertar as Any)
-            .addOnSuccessListener {
-                resultado =  true
-            }
-            .addOnFailureListener {
-                resultado = false
-            }
-        return resultado
+            .document(id.toString())
+            .set(datosInsertar)
     }
 
-    fun eliminarFirebase(idElegido: String):Boolean {
-        var resultado = false
+    fun eliminarFirebase(idElegido: String) {
         baseRemota.collection("NOTAS")
             .document(idElegido)
             .delete()
-            .addOnSuccessListener {
-                resultado =  true
-            }
-            .addOnFailureListener {
-                resultado = false
-            }
-        return resultado
     }
 
-    fun actualizaFirebase(idElegido: String):Boolean {
-        var resultado = false
+    fun actualizaFirebase(idElegido: String) {
         baseRemota.collection("NOTAS")
             .document(idElegido)
             .update("TITULO",titulo)
-            .addOnSuccessListener {
-                resultado = true
-            }
         baseRemota.collection("NOTAS")
             .document(idElegido)
             .update("CONTENIDO",contenido)
-            .addOnSuccessListener {
-                resultado = true
-            }
         baseRemota.collection("NOTAS")
             .document(idElegido)
             .update("HORA",hora)
-            .addOnSuccessListener {
-                resultado = true
-            }
         baseRemota.collection("NOTAS")
             .document(idElegido)
             .update("FECHA",fecha)
-            .addOnSuccessListener {
-                resultado = true
-            }
-        return resultado
     }
 
-    fun selectAllFirestore():ArrayList<Nota>{
-        var notas = ArrayList<Nota>()
+    fun selectAllFirestore(){
+        var listaFirestore = ArrayList<Nota>()
+        var listaLocal = selectAll()
         baseRemota.collection("NOTAS")
             .addSnapshotListener{querySnapshot,error->
                 if(error!=null){
@@ -169,17 +156,71 @@ class Nota(p:Context) {
                         .setPositiveButton("OK"){s,i->}
                         .show()
                     return@addSnapshotListener
-                }
+                }//if END
+                listaFirestore.clear()
                 for (document in querySnapshot!!){
                     val nota = Nota(pointer)
-                    nota.id = document.getString("ID")!!.toInt()
+                    nota.id = document.id.toInt()
                     nota.titulo = document.getString("TITULO")!!
                     nota.contenido = document.getString("CONTENIDO")!!
                     nota.hora = document.getString("HORA")!!
                     nota.fecha = document.getString("FECHA")!!
-                    notas.add(nota)
+                    listaFirestore.add(nota)
+                }//for END
+                //Elimina los documentos que no esten en local pero si en Firestore
+                listaFirestore.forEach {
+                    //Busca si id de firestore esta en LOCAL
+                    if(!consultNota(it.id!!)){
+                        it.eliminarFirebase(it.id.toString())
+                    }
+                }//forEach END
+                //Agrega los documentos que esten en local pero no en firestore
+                if (!listaFirestore.isEmpty()){
+                    var listaInsert = selectIDMAYORA(listaFirestore[listaFirestore.size-1].id.toString())
+                    listaInsert.forEach { it.insertarFirebase() }
+                }else{
+                    listaLocal.forEach{it.insertarFirebase()}
                 }
-            }
+                //actualiza los documentos que se modifican en local a firestore
+                comparaUpdate(listaFirestore,listaLocal)
+                Toast.makeText(pointer,"SE SINCRONIZO CON EXITO", Toast.LENGTH_LONG).show()
+            }//addSnapshotListener END
+    }//selectAllFirestore END
+
+    private fun selectIDMAYORA(idFirestore: String):ArrayList<Nota> {
+        var notas = ArrayList<Nota>()
+        val tabla = BaseDatos(pointer,"BDNOTAS",null,1).readableDatabase
+        val cursor = tabla.rawQuery("SELECT * FROM NOTA WHERE ID > ${idFirestore}",null)
+        if(cursor.moveToFirst()){
+            do{
+                val nota = Nota(pointer)
+                nota.id = cursor.getInt(0)
+                nota.titulo = cursor.getString(1)
+                nota.contenido = cursor.getString(2)
+                nota.hora = cursor.getString(3)
+                nota.fecha = cursor.getString(4)
+                notas.add(nota)
+            }while (cursor.moveToNext())
+        }//end if
         return notas
     }
+
+    fun comparaUpdate(listaFirebase: ArrayList<Nota>, listaLocal: ArrayList<Nota>) {
+        //Compara las listas para ver que dato se modificara
+        listaLocal.forEach {
+            var notaLeida = it
+            //Revisa las notas que se actualizaran
+            (0..listaFirebase.size-1).forEach{
+                //Compara si titulo o contenido son diferentes
+                if(notaLeida.id == listaFirebase[it].id && (
+                            notaLeida.titulo != listaFirebase[it].titulo ||
+                                    notaLeida.contenido != listaFirebase[it].contenido
+                            )){
+                    //Se agrega la nota al arreglo cuando se encontro una modificacion
+                    notaLeida.actualizaFirebase(notaLeida.id.toString())
+                }
+            }
+        }
+    }
+
 }
